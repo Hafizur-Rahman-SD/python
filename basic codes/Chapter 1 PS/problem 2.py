2e8b8ae06d33080a98dc582d2f5f05f65fe39118
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-05-07 12:00:10 +0600

    h

---
+++ basic codes/Chapter 1 PS/problem 2.py
@@ -0,0 +1,10 @@
+"""
+Here have another solution 
+1. open your command promt from your computer
+2. type python and press enter
+3. type the following code and press enter
+"""
+
+
+for i in range(1, 11):
+    print(f"7 x {i} = {7 * i}")
