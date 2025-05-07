61d01d0bb755b68355b91641549920d9971451e6
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-05-07 11:57:25 +0600

    up

---
+++ basic codes/modeul.py
@@ -0,0 +1,21 @@
+import pyjokes 
+# This code imports the pyjokes module and uses it to get a random joke.
+# It then prints the joke to the console.
+joke= pyjokes.get_joke()
+print(joke) 
+
+#this is a single line comment
+"""
+this is multi line comment
+i am Hafizur Rahman
+i am a software engineer        
+i am a python developer
+i am a data scientist   
+i am a data analyst
+i am a data engineer
+i am a machine learning engineer
+i am a deep learning engineer
+i am a web developer
+i am a full stack developer
+
+"""
