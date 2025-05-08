a326179ed6e4060c0334c0bcde0097fe33ac2412
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-05-08 08:39:06 +0600

    Create input.py

---
+++ Variable and data type/input.py
@@ -0,0 +1,13 @@
+# To see the input option got to terminal and gives a pomt python (your file name.py)
+# press enter buttion 
+#Then gives the input in the terminal and press enter buttion again
+# # Example of input in python
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))  
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+
+print("Sum is ", a + b)
+print("Difference is ", a - b)
