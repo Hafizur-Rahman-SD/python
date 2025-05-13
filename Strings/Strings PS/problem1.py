96b7d25e55c30965d8dfba913768a69f43ef92f0
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-05-13 17:46:55 +0600

    h

---
+++ Strings/Strings PS/problem1.py
@@ -0,0 +1,7 @@
+
+
+
+name = input("Enter your name: ")
+
+print (f"Hello {name}, welcome to the world of Python!") 
+#this is a simple program that takes a name as input and prints a welcome message.
---
+++ Strings/Strings PS/problem2.py
@@ -0,0 +1,8 @@
+
+
+
+letter = '''Dear <|Name|>, 
+\t You are selected! 
+<|Date|> '''
+
+print(letter.replace("<|Name|>", "Hafizur").replace("<|Date|", "22-04-2001"))
