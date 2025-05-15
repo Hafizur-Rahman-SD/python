63e449be2b57fd6d51b137838c45adb29e46bee4
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-05-15 15:28:47 +0600

    u

---
+++ Dictonary & sets/Chapter5 PS/p6solve.py
@@ -0,0 +1,27 @@
+
+
+d = {}
+
+name = input("Enter friends name 1: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name 2: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name 3: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name 4: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name 5: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+
+
+print(d)
