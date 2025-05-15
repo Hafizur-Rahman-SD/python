ba233f5772ae0fb84d2b4bf91c361e0f8a9940e3
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-05-15 15:27:49 +0600

    u

---
+++ Dictonary & sets/Chapter5 PS/P1solve.py
@@ -0,0 +1,17 @@
+
+
+# for run this code write in your VS code terminal python P1solve.py(your file name) then press enter
+# If your search word is not in the dictionary then it will give you an error
+words = {
+    "sahajjo": "Help",
+    "chair": "Chair",
+    "bilai": "Cat",
+    "daka": "call",
+    "bari": "Home",
+    "gari": "Car",  
+    "Ghori": "watch",
+}
+
+word = input("Enter the word you want meaning of: ")
+
+print(words[word])
