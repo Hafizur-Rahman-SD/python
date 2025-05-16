d871740258aba3043e169d9d2fe0521eb0f75b9e
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-05-16 21:25:40 +0600

    update code

---
+++ Lists & Tuples/lists.py
@@ -0,0 +1,22 @@
+
+
+lists =["apple", 7, 13.11, True, None, 3.14, "banana", 42, False, "cherry", 0.5]
+# This code defines a list called 'lists' that contains a mix of strings, integers, floats, booleans, and None values.
+
+print(lists)
+# This code prints the contents of the 'lists' variable to the console.
+
+print(type(lists))
+# This code prints the type of the 'lists' variable to the console, which will be <class 'list'>.
+
+print (lists[6])
+# This code prints the element at index 6 of the 'lists' variable, which is "banana".
+
+print (lists[0:3])
+# This code prints a slice of the 'lists' variable from index 0 to index 2 (inclusive), which will be ['apple', 7, 13.11].
+
+
+lists[0] = "orange"
+# # Unlike Strings lists are mutable
+
+
---
+++ Lists & Tuples/lists_methods.py
@@ -0,0 +1,33 @@
+
+
+lists =["apple", 7, 13.11, True, None, 3.14, "banana", 42, False, "cherry", 0.5]
+# This code defines a list called 'lists' that contains a mix of strings, integers, floats, booleans, and None values.
+
+print(lists)
+# This code prints the contents of the 'lists' variable to the console.
+
+lists.append("HAFIZUR") # This code appends the string "HAFIZUR" to the end of the 'lists' variable.
+
+print(lists) #print new list. 
+
+
+
+#Methods of list
+
+hafizur =[ 2,676,5,687,665,443,343,464,65,34]
+hafizur.sort() # This code sorts the 'hafizur' list in ascending order.
+print(hafizur) # This code prints the sorted 'hafizur' list to the console.
+
+hafizur.append("0713") #- Adds an element to the end of the list.
+print(hafizur) # This code prints the updated 'hafizur' list to the console.
+
+
+# 2. extend() - Adds multiple elements to the end of the list.              
+# 3. insert() - Inserts an element at a specified position in the list.
+# 4. remove() - Removes the first occurrence of a specified element from the list.
+
+# 5. pop() - Removes and returns the element at a specified position in the list (default is the last element).
+# 6. clear() - Removes all elements from the list.
+# 7. index() - Returns the index of the first occurrence of a specified element in the list.
+
+
