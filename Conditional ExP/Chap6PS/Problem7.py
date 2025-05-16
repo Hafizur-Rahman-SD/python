7e6f56d3a29923eb8d8c671b275a34652909b333
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-05-16 21:28:18 +0600

    update code

---
+++ Conditional ExP/Chap6PS/Problem7.py
@@ -0,0 +1,12 @@
+
+
+post = input("Enter the post: ")
+
+
+
+if("hafizur" in post.lower()):
+    print("This post is talking about hafizur")
+
+else:
+    print("This post is not talking about hafizur")
+# Compare this snippet from Problem2.py:
