d7ed8abe2118258dd8ba6a5875e5536e0102bb80
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-05-20 21:10:44 +0600

    Create Arithmatic_operator.py

---
+++ Operators/Arithmatic_operator.py
@@ -0,0 +1,27 @@
+
+
+#My Variables 
+x=20
+y=50
+
+# Addition 2 number from var. 
+print ("Addition:", x+y)
+
+#Substraction 
+print ("Subtraction", x-y)
+
+#Multiplecation
+print ("Multiplecation", x*y)
+
+
+# devision 
+print ("Devision:",x/y)
+
+
+#Floor Devision
+print ("Floor Division", x//y)
+
+#modulus
+print("Modulus:", x%y)
+# square 2 number 
+print ("Exponeatiation", x**y)
