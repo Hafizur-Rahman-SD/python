4e5ceae49753bf7ec3af321fec69a6a31b5e99e4
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-05-20 17:34:27 +0600

    Create type().py

---
+++ Variable and data type/type().py
@@ -0,0 +1,15 @@
+# Define variables with different data types
+flot = 4.5  #float number 
+integer = 314  # integer number 
+string = "Hello, World!" #string 
+list = [1, 2, 3,4,5]   # list 
+dictnary = {'Hafizur': 'Rahman'}   #Dictonary type 
+boolean = True     #boolean type 
+
+# Get and print the type of each variable
+print(type(flot))   
+print(type(integer)) 
+print(type(string))   
+print(type(list))     
+print(type(dictnary))     
+print(type(boolean))
