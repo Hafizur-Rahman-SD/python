aba08ed3cf00409665a07e10de3bd421f4eec364
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-05-20 21:20:09 +0600

    Create Relational_Operator.py

---
+++ Operators/Relational_Operator.py
@@ -0,0 +1,26 @@
+
+#2 variable take x and y 
+x=178
+y= 188
+
+# Check if x is greater than y (returns False)
+print (x>y)
+
+# Check if x is less than y (returns True)
+print (x<y)
+
+
+# Check if x is equal to y (returns False)
+print (x==y)
+
+# Check if x is not equal to y (returns True)
+print (x!=y)
+
+# Check if x is greater than or equal to y (returns False)
+print (x>=y)
+
+# Check if x is less than or equal to y (returns True)
+print (x<=y)
+
+
+
