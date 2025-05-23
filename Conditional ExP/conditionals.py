d2cb50cac40d5384c75e56c3b3b254fe084c0385
Hafizur Rahman <hafizurrahman5004@gmail.com>
2025-05-23 14:39:37 +0600

    Update conditionals.py

--- Conditional ExP/conditionals.py
+++ Conditional ExP/conditionals.py
@@ -1,11 +1,11 @@
  
 
-#how to take input from user.int must use beacuse without it it will take string input
+#how to take input from user.int must use beacuse without it it will take string input.
 a= int (input("Enter your age number: "))
 
 if (a>18):   # If statenment 
 	print ("You are eligible to vote")
-	print ("Its Good for you")
+	print ("Its Good for your Country")
 	
     
 
@@ -15,7 +15,7 @@ elif(a==0):
 elif (a<0):
 	print ("You are enter an invalide age -number not your age ")
 else:   
-	print ("You are not eligible to vote")
+	print ("You are not eligible for vote")
 # The above code checks if a person is eligible to vote based on their age.
 
   
